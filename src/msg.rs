21e531a6ab9196e4a87e5c0bd6c96eb11dd971d6
Rogelio Morrell <molekilla@gmail.com>
2021-07-21 15:25:52 -0500

    adding msg models

--- src/msg.rs
+++ src/msg.rs
@@ -1,27 +1,56 @@
+
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
-    pub count: i32,
+    // add InitMsg parameters here
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Increment {},
-    Reset { count: i32 },
+    // add HandleMsg types here
+    CreateFileBlock {
+        payload: Vec<u8>,
+        sig: Vec<u8>,
+        path: String,
+        tag: String,
+        contentType: String,
+    },
+    ReadFileBlock {
+        cid: String,    
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    // add QueryMsg types here
+
 }
 
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+/// Responses from handle function
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum HandleAnswer {
+    // add HandleMsg response types here
+    CreateFileBlock{
+        cid: String,
+    },
+    ReadFileBlock{
+        
+        payload: Vec<u8>,
+        sig: Vec<u8>,
+        path: String,
+        tag: String,
+        contentType: String,
+    }
+}
+
+/// Responses from query function
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryAnswer {
+    // add QueryMsg response types here
 }
--- src/state.rs
+++ src/state.rs
@@ -1,21 +1,40 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr, Storage};
-use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+use std::{any::type_name};
+use serde::{Deserialize, Serialize};
+use cosmwasm_std::{Storage, ReadonlyStorage, StdResult, StdError};
+use serde::de::DeserializeOwned;
+use secret_toolkit::serialization::{Bincode2, Serde};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct State {
-    pub count: i32,
-    pub owner: CanonicalAddr,
+    pub max_size: u16,
+    pub reminder_count: u64,
 }
 
-pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
-    singleton(storage, CONFIG_KEY)
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct FileBlock {
+    pub content: Vec<u8>,
+    pub timestamp: u64,
 }
 
-pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
-    singleton_read(storage, CONFIG_KEY)
+pub fn save<T: Serialize, S: Storage>(storage: &mut S, key: &[u8], value: &T) -> StdResult<()> {
+    storage.set(key, &Bincode2::serialize(value)?);
+    Ok(())
 }
+
+pub fn load<T: DeserializeOwned, S: ReadonlyStorage>(storage: &S, key: &[u8]) -> StdResult<T> {
+    Bincode2::deserialize(
+        &storage
+            .get(key)
+            .ok_or_else(|| StdError::not_found(type_name::<T>()))?,
+    )
+}
+
+pub fn may_load<T: DeserializeOwned, S: ReadonlyStorage>(storage: &S, key: &[u8]) -> StdResult<Option<T>> {
+    match storage.get(key) {
+        Some(value) => Bincode2::deserialize(&value).map(Some),
+        None => Ok(None),
+    }
+}    
